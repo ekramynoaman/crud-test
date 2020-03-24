b6ef7776ae1137737915a39b579c5ebf4e64b6b2
EkramyNo3man <Ekramy.no3man@gmail.com>
2020-03-24 16:17:47 +0200

    for test

---
+++ js/main.js
@@ -0,0 +1,91 @@
+let productContainer;
+if (localStorage.getItem("productsData") == null) {
+  productContainer = [];
+
+
+}
+else {
+  productContainer = JSON.parse(localStorage.getItem("productsData"));
+  displayProduct();
+}
+function addproduct() {
+  let ProductName = document.getElementById("ProductNameInp").value;
+  let ProductPrice = document.getElementById("ProductPriceInp").value;
+  let ProductCategory = document.getElementById("ProductCategoryInp").value;
+  let ProductDesc = document.getElementById("ProductDescInp").value;
+
+  let product = {
+    name: ProductName,
+    price: ProductPrice,
+    category: ProductCategory,
+    description: ProductDesc
+  };
+  productContainer.push(product);
+  localStorage.setItem("productsData", JSON.stringify(productContainer))
+  displayProduct();
+}
+
+
+
+function displayProduct() {
+  let temp = ``;
+  for (let i = 0; i < productContainer.length; i++) {
+    temp += `
+    <div class="col-lg-3 col-md-4 col-sm-6">
+      <div class="products mb-4 mt-4">
+        <img src="https://placehold.it/300x250/000099/ffffff?text=Product" class="img-fluid" />
+        <h4 class="py-2">` + productContainer[i].name + `
+          <span class="ml-3 badge badge-info">` + productContainer[i].category + `</span>
+        </h4>
+        <span class="products-price">` + productContainer[i].price + `</span>
+        <p class="products-desc">` + productContainer[i].description + `</p>
+        <button onclick="deleteProduct(`+ i + `)" type="button" class="btn btn-outline-danger btn-sm">Delete</button>
+          <a  onclick="updateProduct(`+ i +`)" type="button" class="btn btn-outline-info btn-sm">Update</a>
+      </div>
+    </div>`;
+  }
+
+  document.getElementById("productRow").innerHTML = temp;
+
+}
+
+function searchProduct(term) {
+  let temp = ``;
+
+  for (let i = 0; i < productContainer.length; i++) {
+
+    if (productContainer[i].name.toLowerCase().includes(term.toLowerCase()) || productContainer[i].category.toLowerCase().includes(term.toLowerCase()) || productContainer[i].price.includes(term)) {
+
+      temp += `
+      <div class="col-lg-3 col-md-4 col-sm-6">
+        <div class="products">
+          <img src="https://placehold.it/300x250/000099/ffffff?text=Product" class="img-fluid" />
+          <h4 class="py-2">` + productContainer[i].name + `
+            <span class="ml-3 badge badge-info">` + productContainer[i].category + `</span>
+          </h4>
+          <p class="products-desc">` + productContainer[i].description + `</p>
+          <span class="products-price">` + productContainer[i].price + `</span>
+        </div>
+      </div>`;
+    }
+
+  }
+
+  document.getElementById("productRow").innerHTML = temp;
+}
+
+function deleteProduct(indx) {
+  let deleted = productContainer.splice(indx, 1);
+  localStorage.setItem("productsData", JSON.stringify(productContainer))
+  displayProduct();
+}
+
+function updateProduct(indx) {
+    let productId = indx;
+    localStorage.setItem("productIndex", JSON.stringify(productId));
+    // e.target.setAttribute('href', './update_product');
+
+    window.location.href = './updata_product.html'+ productId;
+
+
+}
---
+++ js/update.js
@@ -0,0 +1,44 @@
+
+// let ProductName = document.getElementById("ProductNameInp");
+// let ProductPrice = document.getElementById("ProductPriceInp");
+// let ProductCategory = document.getElementById("ProductCategoryInp");
+// let ProductDesc = document.getElementById("ProductDescInp");
+
+
+
+
+
+// let productContainer;
+// if (localStorage.getItem("productsData") == null) {
+//   console.log('storge is empty');
+ 
+
+
+// }
+// else {
+//   productContainer = JSON.parse(localStorage.getItem("productsData"));
+
+
+
+// let productIndex = JSON.parse(localStorage.getItem("productdata"));
+// let productToUpdate = productContainer[productIndex];
+
+
+// ProductName.value = productToUpdate.name;
+// ProductPrice.value = productToUpdate.price;
+// ProductCategory.value = productToUpdate.category;
+// ProductDesc.value = productToUpdate.description;
+
+
+// function saveProduct() {
+// productToUpdate.name = ProductName.value;
+// productToUpdate.price = ProductPrice.value;
+// productToUpdate.category = ProductCategory.value;
+// productToUpdate.description = ProductDesc.value;
+
+// }
+
+
+// }
+
+
